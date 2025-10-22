c724e465e741950c3039f49d13929de244de06e4
Kriscra <108287401+Kriscra@users.noreply.github.com>
2025-10-22 04:35:40 +0300

    Add files via upload

---
+++ src/main/java/com/rache/racheUtils/RacheUtils.java
@@ -0,0 +1,17 @@
+package com.rache.racheUtils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RacheUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
